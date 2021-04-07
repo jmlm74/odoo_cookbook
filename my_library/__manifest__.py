4eebcf3ac839f98e878e1ff12bb2b3f6d5f7896b
jmlm74 <jmlm74@gmail.com>
2021-04-07 21:49:26 +0200

    Chap4 • Defining the model representation and order • Adding data fields to a model • Using a float field with configurable precision • Adding a monetary field to a model • Adding relational fields to a model • Adding a hierarchy to a model

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -16,7 +16,8 @@ Description related to library.
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
-        'views/library_book.xml'
+        'views/library_book_categ.xml',
+        'views/library_book.xml',
     ],
 
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -10,14 +10,65 @@ class LibraryBook(models.Model):
 
     short_name = fields.Char('Short Title', required=True)
     name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many('res.partner', string='Authors')
 
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision decimals,
+    )
+    cost_price = fields.Float(
+        'Book Cost', digits='Book Price')
+    retail_price = fields.Monetary(
+        'Retail Price',
+        # optional: currency_field='currency_id',
+    )
+
+    publisher_id = fields.Many2one(
+        'res.partner', string='Publisher',
+        # optional:
+        ondelete='set null',
+        context={},
+        domain=[],
+    )
+    author_ids = fields.Many2many('res.partner', string='Authors')
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency')
+    category_id = fields.Many2one('library.book.category')
+
+    """
+    Methods
+    """
     def name_get(self):
         """" correspond au _str """
         result = []
-
         for record in self:
             rec_name = "%s (%s)" % (record.name, record.date_release)
         result.append((record.id, rec_name))
         return result
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    published_book_ids = fields.One2many(
+        'library.book', 'publisher_id',
+        string='Published Books')
+
+    authored_book_ids = fields.Many2many(
+        'library.book',
+        string='Authored Books',
+        # relation='library_book_res_partner_rel',
+        # optional
+    )
+
