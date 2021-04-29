0a6d897ba24d5f580ae552ac8a1ea6b0ab98d332
jmlm74 <jmlm74@gmail.com>
2021-04-29 21:14:25 +0200

    - Sorting recordsets

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -228,6 +228,10 @@ class LibraryBook(models.Model):
     def get_author_names(self, books):
         return books.mapped('author_ids.name')
 
+    @api.model
+    def sort_books_by_date(self, books):
+        return books.sorted(key='release_date')
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     _order = 'name'
