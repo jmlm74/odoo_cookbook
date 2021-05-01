2ccd1c6c839942131753772bd2453017837404eb
jmlm74 <jmlm74@gmail.com>
2021-05-01 21:52:46 +0200

    - Using external IDs and namespaces

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -18,6 +18,7 @@ Description related to library.
         'security/ir.model.access.csv',
         'views/library_book.xml',
         'views/library_book_categ.xml',
+        'data/data.xml',
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
